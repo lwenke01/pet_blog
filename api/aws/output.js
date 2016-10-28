a8360bdf7f4977412171a6fa5b2b2400e1368745
lwenke01 <lwenke@gmail.com>
2016-10-28 16:36:43 -0700

    building out the backend on aws

--- api/aws/output.js
+++ api/aws/output.js
@@ -1,13 +1,14 @@
 'use strict';
 
 var AWS = require('aws-sdk');
+// var sqs_config = require('.config');
 
 var sqs = new AWS.SQS({region:'us-west-2'});
 
 var msg = { payload: 'hey hey' };
 
 var sqsParams = {
-  MessageBody: JSON.stringify({msg}),
+  MessageBody: JSON.stringify(msg),
   QueueUrl: 'https://sqs.us-west-2.amazonaws.com/746604731128/MyQueue'
 };
 
